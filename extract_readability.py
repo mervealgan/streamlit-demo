3ab748563338fc8e16f825f322fbfd7f4d19a65a
mervealgan <159494063+mervealgan@users.noreply.github.com>
2025-07-09 14:20:57 +0200

    Add files via upload

--- extract_readability.py
+++ extract_readability.py
@@ -3,13 +3,18 @@ import numpy as np
 import spacy
 import readability
 
+# Chargement du modèle spaCy pour le français
 nlp = spacy.load("fr_core_news_sm")
 
+# Extraction des mesures de lisibilité
 def get_features(text, lang='fr'):
     doc = nlp(text)
+    
+    # Reformater le texte
     tokenized = '\n\n'.join(' '.join(token.text for token in sent) for sent in doc.sents)
     results = readability.getmeasures(tokenized, lang=lang, merge=True)
 
+    # Supprimer les mesures qu'on n'utilise pas
     for key in [
         'Kincaid', 'ARI', 'Coleman-Liau', 'FleschReadingEase',
         'GunningFogIndex', 'SMOGIndex', 'DaleChallIndex',
@@ -19,12 +24,16 @@ def get_features(text, lang='fr'):
 
     return results
 
+# Calcul des différences de lisibilité entre deux phrases
 def extract_readability_features(original, simplified):
     ori_feats = get_features(original)
     sim_feats = get_features(simplified)
 
-    ori_df = pd.Series(ori_feats).add_prefix("ori_")
-    sim_df = pd.Series(sim_feats).add_prefix("sim_")
+    # Convertir les résultats en Series pandas
+    ori_df = pd.Series(ori_feats)
+    sim_df = pd.Series(sim_feats)
+
+    # Calcul de la différence (simplifiée - originale)
     diff_df = sim_df - ori_df
     diff_df = diff_df.add_prefix("diff_")
 
